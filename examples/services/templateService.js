54c81ec1d1b29d420c52f55b7e3462136f4b7ab3
Hstarorg <hm910705@163.com>
2017-08-20 23:34:11 +0800

    Fixed reference address

--- examples/services/templateService.js
+++ examples/services/templateService.js
@@ -3,6 +3,7 @@ const baseTemplate = `
 <html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <base href="/">
     <link href="//cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <!-- Bootstrap 3.3.7 -->
     <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
@@ -18,7 +19,7 @@ const baseTemplate = `
   <body>
   {{htmlCode}}
   <script src="//cdn.bootcss.com/vue/2.4.2/vue.js"></script>
-  <script src="/lib/vue-lte-ui.js"></script>
+  <script src="lib/vue-lte-ui.js"></script>
   <script>
     {{jsCode}}
   </script>
